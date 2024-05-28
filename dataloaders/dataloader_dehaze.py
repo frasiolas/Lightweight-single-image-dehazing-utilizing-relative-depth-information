3a746e57fbaccc5e6e9d7214df6cd165658a33b4
frasiolas <136797058+frasiolas@users.noreply.github.com>
2024-05-28 15:00:49 +0300

    Update dataloader_dehaze.py

--- dataloaders/dataloader_dehaze.py
+++ dataloaders/dataloader_dehaze.py
@@ -19,13 +19,6 @@ def renormalize(tensor):
     maxTo=1
     return minTo + (maxTo - minTo) * ((tensor - minFrom) / (maxFrom - minFrom))
     
-
-
-import random
-import numpy as np
-import random
-import numpy as np
-
 class RESIDE_train(Dataset):
 
   def __init__(self):
@@ -93,219 +86,11 @@ class RESIDE_SOTS(Dataset):
     clear = clear_GT_trans(clear)
     haze = hazy_transforms(haze)
     
-   
-    
-  
     return {'Clear': clear, 'Hazy': haze}
 
   def __len__(self):
     return self.n_samples
   
-
-
-class CustomDataset:
-    def __init__(self):
-        self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\Reside_train.csv', sep = ',')
-        self.n_samples = self.data.shape[0]
-
-        self.crop_transform = transforms.RandomCrop((256, 256))
-
-        self.transforms = transforms.ToTensor()
- 
-    def __getitem__(self, index):
-        path_clear = self.data._get_value(index, 'Clear', takeable=False)
-        clear = Image.open(path_clear)
-        path_hazy = self.data._get_value(index, 'Hazy', takeable=False)
-        haze = Image.open(path_hazy)
-        path_depth = self.data._get_value(index, 'Depth', takeable=False)
-        depth = Image.open(path_depth)
-
-        horizontal_flip = transforms.RandomHorizontalFlip(p=1)
-        if random.random() > 0.5:
-          clear = horizontal_flip(clear)
-          haze = horizontal_flip(haze)
-          depth = horizontal_flip(depth)
-
-       # Random crop
-        i, j, h, w = transforms.RandomCrop.get_params(
-            clear, output_size=(256, 256))
-        clear = crop(clear, i, j, h, w)
-        haze = crop(haze, i, j, h, w)
-        depth = crop(depth, i, j, h, w)
-
-
-        clear = self.transforms(clear)
-        haze = self.transforms(haze)
-        depth = self.transforms(depth)
-    
-
-          
-        return {'Clear': clear, 'Hazy': haze, 'Depth':depth}
-
-    def __len__(self):
-        return self.n_samples
-
-
-  
-class NH_HAZE_train:
-    def __init__(self):
-        self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\NH-HAZE_train.csv', sep = ',')
-        self.n_samples = self.data.shape[0]
-        self.transforms = transforms.ToTensor()
-    
-
-    def __getitem__(self, index):
-        path_clear = self.data._get_value(index, 'Clear', takeable=False)
-        clear = Image.open(path_clear)
-        path_hazy = self.data._get_value(index, 'Hazy', takeable=False)
-        haze = Image.open(path_hazy)
-        path_depth = self.data._get_value(index, 'Depth', takeable=False)
-        depth = Image.open(path_depth)
-
-      
-        horizontal_flip = transforms.RandomHorizontalFlip(p=1)
-        if random.random() > 0.5:
-          clear = horizontal_flip(clear)
-          haze = horizontal_flip(haze)
-          depth = horizontal_flip(depth)
-
-       # Random crop
-        i, j, h, w = transforms.RandomCrop.get_params(clear, output_size=(512, 512))
-        clear = crop(clear, i, j, h, w)
-        haze = crop(haze, i, j, h, w)
-        depth = crop(depth, i, j, h, w)
-
-        clear = self.transforms(clear)
-        haze = self.transforms(haze)
-        depth = self.transforms(depth)
-    
-
-          
-        return {'Clear': clear, 'Hazy': haze, 'Depth':depth}
-    
-    def __len__(self):
-      return self.n_samples
-  
-
-
-class NH_HAZE_train_full:
-
-  def __init__(self):
-    self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\NH-HAZE_train.csv', sep = ',')
-    self.n_samples = self.data.shape[0]
-
-
-  def __getitem__(self, index):
-    path_clear = self.data._get_value(index, 'Clear', takeable=False)
-    clear = Image.open(path_clear)
-
-    path_hazy = self.data._get_value(index, 'Hazy', takeable=False)
-    haze = Image.open(path_hazy)
-
-    path_depth = self.data._get_value(index, 'Depth', takeable=False)
-    depth = Image.open(path_depth)
-
-    hazy_transforms = transforms.Compose([
-       transforms.Resize((480,480)),
-       transforms.ToTensor(),
-       #transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    ])
-    #resize =  transforms.Resize((448,608))
-    to_tensor = transforms.ToTensor()
-    
-    horizontal_flip = transforms.RandomHorizontalFlip(p=1)
-    if random.random() > 0.5:
-      clear = horizontal_flip(clear)
-      haze = horizontal_flip(haze)
-      depth = horizontal_flip(depth)
-
-    clear = hazy_transforms(clear)
-    haze = hazy_transforms(haze)
-    depth = hazy_transforms(depth)
-    depth = depth.mean(dim=0)
-    depth = depth.unsqueeze(0) 
-   
-  
-    return {'Clear': clear, 'Hazy': haze, 'Depth':depth}
-
-  def __len__(self):
-    return self.n_samples
-class NH_HAZE_all_res:
-
-  def __init__(self):
-    self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\NH-HAZE_train.csv', sep = ',')
-    self.n_samples = self.data.shape[0]
-
-
-  def __getitem__(self, index):
-    path_clear = self.data._get_value(index, 'Clear', takeable=False)
-    clear = Image.open(path_clear)
-
-    path_hazy = self.data._get_value(index, 'Hazy', takeable=False)
-    haze = Image.open(path_hazy)
-
-    path_depth = self.data._get_value(index, 'Depth', takeable=False)
-    depth = Image.open(path_depth)
-
-    hazy_transforms = transforms.Compose([
-       transforms.Resize((1184,1600)),
-       transforms.ToTensor(),
-       #transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    ])
-    #resize =  transforms.Resize((448,608))
-    to_tensor = transforms.ToTensor()
-    
-    horizontal_flip = transforms.RandomHorizontalFlip(p=1)
-    if random.random() > 0.5:
-      clear = horizontal_flip(clear)
-      haze = horizontal_flip(haze)
-      depth = horizontal_flip(depth)
-
-    clear = to_tensor(clear)
-    haze = hazy_transforms(haze)
-    depth = to_tensor(depth)
-    depth = depth.mean(dim=0)
-    depth = depth.unsqueeze(0) 
-   
-  
-    return {'Clear': clear, 'Hazy': haze, 'Depth':depth}
-
-  def __len__(self):
-    return self.n_samples
-  
-class NH_HAZE_val:
-
-  def __init__(self):
-    self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\NH-HAZE_val.csv', sep = ',')
-    self.n_samples = self.data.shape[0]
-
-
-  def __getitem__(self, index):
-    path_clear =  self.data._get_value(index, 'Clear', takeable=False)
-    clear = Image.open(path_clear)
-    path_hazy = self.data._get_value(index, 'Hazy', takeable=False)
-    haze = Image.open(path_hazy)
-    hazy_transforms = transforms.Compose([
-        transforms.Resize((1184,1600)),
-       transforms.ToTensor(),
-    ])
- 
-    clear_GT_trans = transforms.Compose([
-       #transforms.Resize((480,480)),
-       transforms.ToTensor(),
-  
-    ])
-    
-    clear = clear_GT_trans(clear)
-    haze = hazy_transforms(haze)
-
-   
-    return {'Clear': clear, 'Hazy': haze}
-
-  def __len__(self):
-    return self.n_samples
-  
-
 
   
 class RESIDE_train_OTS(Dataset):
@@ -360,7 +145,6 @@ class RESIDE_SOTS_out(Dataset):
     self.data = pd.read_csv(r'D:\PanagiotisFrasiolas\RESCUER\unified_model\csv\RESIDE_SOTS_out.csv', sep = ',')
     self.n_samples = self.data.shape[0]
 
-
   def __getitem__(self, index):
     path_clear =  self.data._get_value(index, 'Clear', takeable=False)
     clear = Image.open(path_clear)
@@ -379,27 +163,8 @@ class RESIDE_SOTS_out(Dataset):
     clear = hazy_transforms(clear)
     haze = hazy_transforms(haze)
     
-   
-    
-  
     return {'Clear': clear, 'Hazy': haze}
 
   def __len__(self):
     return self.n_samples
-  
-#nh_haze_dataset = RESIDE_train_OTS()
-# #Iterate over the samples and print the images
-# for i in range(len(nh_haze_dataset)):
-#     sample = nh_haze_dataset[i]
-#     clear_image = transforms.ToPILImage()(sample['Clear'])
-#     hazy_image = transforms.ToPILImage()(sample['Hazy'])
-#     depth_image = transforms.ToPILImage()(sample['Depth'])
-#     single_channel_grayscale = sample['Depth'].mean(dim=0)
-#     single_channel_grayscale = single_channel_grayscale.unsqueeze(0) 
-#     print(sample['Hazy'].shape)
 
-#     # clear_image.show()
-#     # hazy_image.show()
-#     single_channel_grayscale = transforms.ToPILImage()(single_channel_grayscale)
-#     depth_image.show()
-#     single_channel_grayscale.show()
